e8f2a5b300206be81bbfe71a66f7753991b8dc5d
Justin Barclay <justincbarclay@gmail.com>
2017-02-06 10:35:07 -0700

    Test to see if the json api can respond to an authentication request

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  get 'static_pages/home'
-
-  get 'static_pages/help'
+    get 'static_pages/home'
+    
+    get 'static_pages/help'
 
     root     'static_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+        
+    namespace :api do
+        get 'signup', to: 'users/new'
+        post '/login', to: 'users/authenticate'
+        reousrces  :users
+    end
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
-    assert_response :success
-  end
+    def setup
+        @user =  {
+            email: "jbarclay@ualberta.ca",
+            password: "foobarz"
+        }
+        
+    end
 
+    test "logging in a valid user should be successful" do
+        post "/api/login", params: @user
+        assert response.success?
+    end
+
+    test "should create a new user" do
+        
+    end
 end
