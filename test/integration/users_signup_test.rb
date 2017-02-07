2de96b2bb897fb23f10ced8244e0762b2e934067
Justin Barclay <justincbarclay@gmail.com>
2017-02-07 09:42:47 -0700

    Start testing user creation

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+    test "invalid account infomation should fail" do
+        assert_no_difference 'User.count' do
+            post '/api/user#new', params: {name: "", email: "justin@invalid", password: "short", password_confirmation: "thing"}
+        end
+        assert_response :missing
+    end
+end
