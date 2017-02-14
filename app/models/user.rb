623721b7fee0e2d71aa3efb2659f40c635a1aa87
Justin Barclay <justincbarclay@gmail.com>
2017-02-14 11:46:25 -0700

    More style fixes to line up with Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
   
   validates :password, presence: true, length: { minimum: 7 }
 
