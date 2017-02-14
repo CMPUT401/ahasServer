ab717784aecbee985e9bb77a761bdb114b38077e
Justin Barclay <justincbarclay@gmail.com>
2017-02-14 12:51:35 -0700

    Add patient to routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
         post         'signup', to: 'users#create'
         post         'user_token', to: 'user_token#create'
         resources    :users
+        resources    :patients
     end
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
