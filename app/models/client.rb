55471e67756d59926be3308d2b5ce791ac64e420
mackenzie <mackenziebligh@gmail.com>
2017-02-13 20:51:39 -0700

    add data validations to client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,15 @@
 class Client < ApplicationRecord
   #Ensure client's email is unique
   before_save {email.downcase!}
+ 
+  #Data validations
+  validates :name, presence: true, length: {maximum: 50}
+  validates :address, presence: true, length: {maximum: 50}
+  validates :phoneNumber, presence: true
+  validates :name, presence: true, length: {maximum: 50}
 
-  validates :name, {maximum: 50}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
 
 end
--- test/controllers/client_controller_test.rb
+++ test/controllers/client_controller_test.rb
@@ -8,33 +8,33 @@ class ClientControllerTest < ActionDispatch::IntegrationTest
     name = "Leeroy Jenkins"
     address = "1234 Fake St, Edmonton, Alberta"
     phone = "780-951-9085"
-    email = "leeroyjenkins@gmail.com" 
+    email = "leeroyjenkins@gmail.com"
     licos = 40210.00
     aish = 91231.00
     socialAssistance = 1231.00
     pets = 1
     client = Client.new(name, address, phone, email, licos, aish, socialAssistance, pets)
-    assert_not_equal client, nil 
+    assert_not_equal client, nil
   end
 
   test "create client with bad input" do
     name = "Leeroy Jenkins"
     address = "1234 Fake St, Edmonton, Alberta"
     phone = "780951-85"
-    email = "leeroyjenkins.gmail.com" 
+    email = "leeroyjenkins.gmail.com"
     licos = 40123
     aish =  -10123
     socialAssistance = -12310.00
-    pets = 1 
+    pets = 1
     client = Client.new(name, address, phone, email, licos, aish, socialAssistance, pets)
     assert_equal client, nil
   end
 
-  test "respond to successful POST" do 
+  test "respond to successful POST" do
     name = "Leeroy Jenkins"
     address = "1234 Fake St, Edmonton, Alberta"
     phone = "780-951-9085"
-    email = "leeroyjenkins@gmail.com" 
+    email = "leeroyjenkins@gmail.com"
     licos = 40210.00
     aish = 91231.00
     socialAssistance = 1231.00
@@ -48,11 +48,11 @@ class ClientControllerTest < ActionDispatch::IntegrationTest
     name = "Leeroy Jenkins"
     address = "1234 Fake St, Edmonton, Alberta"
     phone = "780951-85"
-    email = "leeroyjenkins.gmail.com" 
+    email = "leeroyjenkins.gmail.com"
     licos = 40123
     aish =  -10123
     socialAssistance = -12310.00
-    pets = 1 
+    pets = 1
     client = Client.new(name, address, phone, email, licos, aish, socialAssistance, pets)
     client.sendResponse()
     assert_response :fail
