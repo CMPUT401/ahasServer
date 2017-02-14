7f3d009d69fe63de769faeecb88358d333170065
Justin Barclay <justincbarclay@gmail.com>
2017-02-14 11:21:12 -0700

    Minor stylistic changes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,21 @@
 class User < ApplicationRecord
-    #Ensure that the users email is unique by making all emails lowercase
-    before_save { email.downcase! }
+  # Ensure that the users email is unique by making all emails lowercase
+  before_save { email.downcase! }
 
-    validates :name, presence: true, length: {maximum: 50}
+  validates :name, presence: true, length: {maximum: 50}
 
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
-    
-    validates :password, presence: true, length: { minimum: 7 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-    has_secure_password
+  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
+  
+  validates :password, presence: true, length: { minimum: 7 }
 
-    private
-    #digest user password with specific Bcrypt parameters
-    def User.digest(string)
-       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-       BCrypt::Password.create(string, cost: cost)
-    end 
+  has_secure_password
+
+  # digest user password with specific Bcrypt parameters
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
