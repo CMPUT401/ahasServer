05baec85e6b0aba8f5821cb2a4028096c90f06ab
Justin Barclay <justincbarclay@gmail.com>
2017-02-15 09:38:15 -0700

    Constrain API routes to JSON format

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
 
 
-    get              'static_pages/home'
-    get              'static_pages/help'
-    root             'static_pages#home'
+  get              'static_pages/home'
+  get              'static_pages/help'
+  root             'static_pages#home'
 
-    # we want our routes to be /api/routes but not our controllers
-    # this may need to be reevalutated if we get lots of non api
-    # controllers
-    scope :api do
-        post         'signup',     to: 'users#create'
-        post         'user_token', to: 'user_token#create'
-        post         'login',      to: 'user_token#create'
-        resources    :users
-        resources    :patients
+  # we want our routes to be /api/routes but not our controllers
+  # this may need to be reevalutated if we get lots of non api
+  # controllers
+  scope :api do
+    constraints format: :json do
+      post         'signup',     to: 'users#create'
+      post         'user_token', to: 'user_token#create'
+      post         'login',      to: 'user_token#create'
+      resources    :users
+      resources    :patients
     end
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
