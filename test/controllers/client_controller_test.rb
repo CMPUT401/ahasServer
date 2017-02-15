1bd2a3e2433a9c19ea40c0478fcfc08132f1365d
mackenzie <mackenziebligh@gmail.com>
2017-02-15 14:26:04 -0700

    Move integration tests to correct file, UC finished

--- test/controllers/client_controller_test.rb
+++ test/controllers/client_controller_test.rb
@@ -2,28 +2,4 @@ require 'test_helper'
 
 class ClientControllerTest < ActionDispatch::IntegrationTest
 
-  test "respond to successful POST" do
-      
-      post "/client",
-      params: {client: {name:  "Leeroy Jenkins", address: "1234 Fake St, Edmonton, Alberta",\
-                       phone: "7809519085", email: "leeroyjenkins@gmail.com", licos: "40210.00",\
-                       socialAssistance: "78000", pets: "123"}}, headers: authentication_header
-
-    assert_response 201
-  end
-#
-#  test "respond to failed POST" do
-#    name = "Leeroy Jenkins"
-#    address = "1234 Fake St, Edmonton, Alberta"
-#    phone = "780951-85"
-#    email = "leeroyjenkins.gmail.com"
-#    licos = 40123
-#    aish =  -10123
-#    socialAssistance = -12310.00
-#    pets = 1
-#    client = Client.new(name, address, phone, email, licos, aish, socialAssistance, pets)
-#    client.sendResponse()
-#    assert_response :error
-  #  assert JSON.parse(response.body)['errors'].count > 0
-#  end
 end
