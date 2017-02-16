e5b02d6c218cac364addd5c691f19ea3676c9301
Justin Barclay <justincbarclay@gmail.com>
2017-02-15 23:55:53 -0700

    Quick fix for CORS issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   include Knock::Authenticable
   # before_action :authenticate_user
-  
+  def set_acces
+    @response.headers["Access-Control-Allow-Origin"] = "*"
+  end
 end
