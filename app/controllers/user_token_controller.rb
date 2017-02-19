a0145cae1e9b531d3df6701adad4fd70439aa82c
Justin Barclay <justincbarclay@gmail.com>
2017-02-19 12:24:44 -0700

    changing location of thing

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,8 +1,3 @@
 class UserTokenController < Knock::AuthTokenController
-  use Rack::Cors do
-    allow do
-      origins '*' #-> has to be "*" or specific
-      resource '*', headers: :any, methods: [:get, :post, :options]
-    end
-  end
+
 end
