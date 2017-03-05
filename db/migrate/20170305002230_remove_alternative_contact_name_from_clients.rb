1d91006525470e0c466a08f13660517e8334ef86
mackenzie <mackenziebligh@gmail.com>
2017-03-04 18:11:01 -0700

    Try 4 to resolve migration error. Removed remove alternate contact name migration

--- db/migrate/20170305002230_remove_alternative_contact_name_from_clients.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAlternativeContactNameFromClients < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :clients, :alternativeContactName
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305002230) do
+ActiveRecord::Schema.define(version: 20170304225922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,8 @@ ActiveRecord::Schema.define(version: 20170305002230) do
     t.float    "aish"
     t.float    "socialAssistance"
     t.string   "pets"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.string   "alternativeContactPhoneNumber"
-    t.string   "alternativeContactAddress"
-    t.string   "notes"
-    t.string   "alternativeContact2ndPhone"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "alternateContactEmail"
     t.string   "lastName"
     t.string   "alternativeContactLastName"
