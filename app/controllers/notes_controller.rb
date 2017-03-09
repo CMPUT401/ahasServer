df8fd940be4a7bd1e3e3c09aae42925b4853c433
Justin Barclay <justincbarclay@gmail.com>
2017-03-08 22:59:41 -0700

    Finish up testing

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,17 +9,22 @@ class NotesController < ApplicationController
   end
 
   def show
-    @note = Note.find_by(id: params[:id])
-    if @note
-      render status: :success, json: { success: true, note: @note }
+
+    note = Note.find_by id: params[:id]
+    if !note.nil?
+      render status: 200, json: { success: true, note: note }
     else
-      render statis: 404, json: { success: false, error: 'Note not found' }
+      render status: 404, json: { success: false, error: 'Note not found' }
     end
   end
 
   def index
-    @notes = MedicalRecord.find_by(id: params[:medical_record])
-    render state: :success, json: { success: true, notes: notes }
+    puts params.to_json
+
+    record = MedicalRecord.find_by(id: params[:medical_record_id])
+    puts record.notes
+    puts record.notes
+    render state: :success, json: { success: true, notes: record.notes }
   end
   
   private
--- test/integration/notes_test.rb
+++ test/integration/notes_test.rb
@@ -3,31 +3,51 @@ require 'test_helper'
 class NotesTest < ActionDispatch::IntegrationTest
   def setup
     @good = notes(:one)
+    @good.save
     @medical_record = medical_records(:one)
+    @note = {
+      medical_record_id: @medical_record.id,
+      body: 'hello',
+      initials: 'jk rowling'
+    }
   end
 
   test 'posting a valid note' do
-    post "/api/patients/#{@medical_record.patient_id}/medical_records/#{@medical_record.id}/notes", headers: authenticated_header, params: { note: {
-                                                                                                                                               medical_record_id: @medical_record.id,
-                                                                                                                                               body: 'hello',
-                                                                                                                                               initials: 'jk rowling'
-                                                                                                                                             }
-                                                                                                                                           }
+    post "/api/patients/#{@medical_record.patient_id}/medical_records/#{@medical_record.id}/notes", headers: authenticated_header, params: { note: @note }
     
     assert_response 201
     assert JSON.parse(response.body)['success']
   end
 
   test 'posting a invalid note' do
-    post "/api/patients/#{@medical_record.patient_id}/medical_records/#{@medical_record.id}/notes", headers: authenticated_header, params: { note: {
-                                                                                                                                               medical_record_id: @medical_record.id,
-                                                                                                                                               body: '',
-                                                                                                                                               initials: 'jk rowling'
-                                                                                                                                             }
-                                                                                                                                           }
-    
-    assert_response :error
-    assert_not JSON.parse(response.body)['success']
+    @note['body'] = ''
+    assert_no_difference 'Note.count' do
+      post "/api/patients/#{@medical_record.patient_id}/medical_records/#{@medical_record.id}/notes", headers: authenticated_header, params: { note: @note }
+      
+      assert_response :error
+      assert_not JSON.parse(response.body)['success']
+    end
   end
-  
+
+  test 'asking for a valid client' do
+    get "/api/patients/#{@medical_record.patient_id}/medical_records/#{@medical_record.id}/notes/#{@good.id}", headers: authenticated_header
+
+    assert_response :success
+    assert JSON.parse(response.body)['note']['id'] == @good.id
+  end
+
+  test 'asking for an invalid client' do
+    @bad_id = Note.last.id + 1
+    get "/api/patients/#{@medical_record.patient_id}/medical_records/#{@medical_record.id}/notes/#{@bad_id}", headers: authenticated_header
+
+    assert_response 404
+    assert !JSON.parse(response.body)['error'].nil?
+  end
+
+  test 'get index of notes for medical records' do
+    get "/api/patients/#{@medical_record.patient_id}/medical_records/#{@medical_record.id}/notes", headers: authenticated_header
+
+    assert_response 200
+    assert JSON.parse(response.body)['notes'].length > 0
+  end
 end
