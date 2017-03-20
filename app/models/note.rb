7da098a2c2ed1b4bfd9f253491f165c7196c8bb2
Justin Barclay <justincbarclay@gmail.com>
2017-03-20 16:19:08 -0600

    Add alert field to notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,8 @@
 class Note < ApplicationRecord
   belongs_to :medical_record
 
+  validates :is_alert, inclusion: { in: [true, false], message: "%{value} is not a boolean" }
+
   validates :body, presence: true, allow_blank: false
 
   validates :initials, presence: true, allow_blank: false
