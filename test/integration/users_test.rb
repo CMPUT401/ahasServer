b86e6fe1b1be5a64e0aad9e1b5609aa8c85876d9
Justin Barclay <justincbarclay@gmail.com>
2017-03-21 10:24:44 -0600

    Return a single user

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -12,4 +12,20 @@ class UsersTest < ActionDispatch::IntegrationTest
     assert JSON.parse(response.body)['success']
     assert JSON.parse(response.body)['users'].length > 0
   end
+
+  test 'getting a user' do
+    get "/api/users/#{@user.id}", headers: authenticated_header
+
+    assert_response 200
+    assert JSON.parse(response.body)['success']
+    assert JSON.parse(response.body)['user'].keys.length > 0
+  end
+
+  test 'getting a non existant user' do
+    bad_id = @user.id + 1
+    get "/api/users/#{bad_id}", headers: authenticated_header
+
+    assert_response 404
+    assert_not JSON.parse(response.body)['success']
+  end
 end
