139936f889331dc6eaa63c0c0d48265ffeb147bd
Justin Barclay <justincbarclay@gmail.com>
2017-03-22 21:32:59 -0600

    Test all filter types

--- test/integration/images_test.rb
+++ test/integration/images_test.rb
@@ -7,14 +7,41 @@ class ImagesTest < ActionDispatch::IntegrationTest
     @lab_result = images(:two)
     @portrait = images(:three)
   end
-  test 'getting an image should return all images of that type' do
-    get "/api/patients/#{@patient.id}/images/filter/radiograph", headers: authenticated_header
 
+  test 'getting an image with a filter of portrait should return the proper type' do
+    get "/api/patients/#{@patient.id}/images/filter/portrait", headers: authenticated_header
+
+    images = JSON.parse(response.body)['images']
     assert_response 200
-    assert JSON.parse(response.body)['images'].length == 1
+    assert images.length == 1
+    assert images[0]['picture_type'] == 'portrait'
   end
 
+  test 'getting an image with a filter of lab_result should return the proper type' do
+    get "/api/patients/#{@patient.id}/images/filter/lab_result", headers: authenticated_header
+
+    images = JSON.parse(response.body)['images']
+    assert_response 200
+    assert images.length == 1
+    assert images[0]['picture_type'] == 'lab_result'
+  end
+  
+  test 'getting an image with a filter of radiograph should return the proper type' do
+    get "/api/patients/#{@patient.id}/images/filter/radiograph", headers: authenticated_header
+
+    images = JSON.parse(response.body)['images']
+    assert_response 200
+    assert images.length == 1
+    assert images[0]['picture_type'] == 'radiograph'
+  end
+  
   test 'index of images returns all images' do
+    get "/api/patients/#{@patient.id}/images", headers: authenticated_header
+    assert_response 200
+    assert JSON.parse(response.body)['images'].length > 0
+  end
+
+  test 'can of images returns all images' do
     get "/api/patients/#{@patient.id}/images", headers: authenticated_header
     assert_response 200
     assert JSON.parse(response.body)['images'].length > 0
