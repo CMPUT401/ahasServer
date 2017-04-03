e024f28b859f30ecbb80f0157d14c125c6ca3a7b
Justin Barclay <justincbarclay@gmail.com>
2017-04-02 19:20:28 -0600

    Update admins test

--- test/integration/admins_test.rb
+++ test/integration/admins_test.rb
@@ -5,7 +5,7 @@ class AdminsTest < ActionDispatch::IntegrationTest
     @user = users(:justin)
   end
 
-  test "Test inviting a user sends an email" do
+  test 'Test inviting a user sends an email' do
     post '/api/admin/invite', headers: authenticated_admin_header, params: { user: {
                                                                                name: 'Justin Barclay',
                                                                                email: 'justincbarclay@gmail.com',
@@ -17,7 +17,7 @@ class AdminsTest < ActionDispatch::IntegrationTest
     assert JSON.parse(response.body)['success']
   end
 
-    test "Test inviting an Admin sends an email" do
+  test 'Test inviting an Admin sends an email' do
     post '/api/admin/invite', headers: authenticated_admin_header, params: { user: {
                                                                                name: 'Justin Barclay',
                                                                                email: 'justincbarclay@gmail.com',
@@ -28,7 +28,7 @@ class AdminsTest < ActionDispatch::IntegrationTest
     assert_equal invite_email.to[0], 'justincbarclay@gmail.com'
     assert JSON.parse(response.body)['success']
   end
-  test "Test inviting a user with invalid credentials" do
+  test 'Test inviting a user with invalid credentials' do
     post '/api/admin/invite', headers: authenticated_header, params: { user: {
                                                                          name: 'Justin Barclay',
                                                                          email: 'justincbarclay@gmail.com'
@@ -37,15 +37,15 @@ class AdminsTest < ActionDispatch::IntegrationTest
     assert_response 401
   end
 
-  test "Test reseting a user password sends an email" do
+  test 'Test reseting a user password sends an email' do
     post '/api/admin/reset_password', headers: authenticated_admin_header, params: { user: @user.attributes }
     
     invite_email = ActionMailer::Base.deliveries.last
-    assert_equal invite_email.to[0], 'justincbarclay@gmail.com'
+    assert_equal invite_email.to[0], @user.email
     assert JSON.parse(response.body)['success']
   end
 
-  test "Test resetting a user password with invalid credentials" do
+  test 'Test resetting a user password with invalid credentials' do
     post '/api/admin/reset_password', headers: authenticated_header, params: { user: @user.attributes }
     assert_response 401
   end
